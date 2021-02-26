23eb0d3bda275ed1a5f3c773783311741a7e960a
alexandref13 <alexandrefilhoalvo@gmail.com>
2021-02-26 02:16:59 -0300

    first commit and hello world

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_testing/pages/login.dart';
+
+void main() {
+  runApp(GetMaterialApp(
+    home: Login(),
+    getPages: [],
+    debugShowCheckedModeBanner: false,
+  ));
+}
